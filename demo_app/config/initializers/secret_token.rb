edc7df895ea3a6e6a95388f551f75cf6cdc82239
webonise <webonise@webonise-desktop.(none)>
2012-12-18 19:06:16 +0530

    First Apllication of Ruby on Rails

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5baba4e9235a41979d233d36fd93eedb11ae56e90bccc86ec09372ffc269ca5b2414b61d2e420323c76abf644b737613eba9489ec00c8072e8ee7a5679aa9a5e'
